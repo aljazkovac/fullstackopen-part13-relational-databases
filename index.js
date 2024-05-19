42e0a1a60733de32d20854e4bec5f5a8dae07003
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2024-05-19 22:45:57 +0200

    Setup up to using Sequelize.

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const { Sequelize, QueryTypes } = require('sequelize')
+const express = require('express')
+const app = express()
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: process.env.NODE_ENV === 'production' ? {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    } : {},
+});
+
+app.get('/api/notes', async(req, res) => {
+    const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
+    res.json(notes)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
