524d209ffc228edb3e377844781ab9a0c60e6b22
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2024-05-20 23:16:38 +0200

    Finish ex. 13.1, 13.2 and 13.3.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, QueryTypes, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
+app.use(express.json()) // Parse JSON bodies
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: process.env.NODE_ENV === 'production' ? {
@@ -12,9 +13,61 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     } : {},
 });
 
+class Note extends Model {}
+Note.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    important: {
+        type: DataTypes.BOOLEAN
+    },
+    date: {
+        type: DataTypes.DATE
+    }
+}, {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'note'
+})
+
 app.get('/api/notes', async(req, res) => {
-    const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
-    res.json(notes)
+    try {
+        const notes = await Note.findAll()
+        res.json(notes)
+    }
+    catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'Internal server error.' })
+    }
+})
+
+app.post('/api/notes', async (req, res) => {
+    try {
+        const note = await Note.create(req.body)
+        return res.json(note)
+        
+    } catch(error) {
+        return res.status(400).json({error})
+    }
+})
+
+app.get('/api/blogs', async(req, res) => {
+    try {
+        const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+        res.json(blogs)
+        blogs.forEach(b => console.log(`${b.author}: '${b.title}', ${b.likes} likes`))
+    }
+    catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'Internal server error.' })
+    }
 })
 
 const PORT = process.env.PORT || 3001
