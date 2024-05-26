f527d94a00f35d4d5d4a8f02f7ac461cf39e03c7
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2024-05-26 15:38:12 +0200

    Start on login functionality. WIP.

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 // Makes it possible to get rid of all the try-catch blocks in async functions, as well as calls to next(error). 
 require('express-async-errors')
-const errorHandler = require('./middleware/errorHandler')
-const blogsRouter = require('./controllers/blogs')
 const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
+const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
+const usersRouter = require('./controllers/users')
+const errorHandler = require('./middleware/errorHandler')
 
 const app = express()
 app.use(express.json()) // Middleware for parsing JSON.
 app.use('/api/blogs', blogsRouter) // Middleware for route handling.
+app.use('/api/users', usersRouter) // Middleware for user handling.
+app.use('/api/login', loginRouter) // Middleware for login handling.
 app.use(errorHandler) // Middleware for error handling.
 
 const start = async () => {
